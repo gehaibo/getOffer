8a1bbd0e554ce922c6d2a5df02632868061a208c
gehaibo <ghbhust@qq.com>
2016-12-22 20:26:49 +0800

    Initial commit

---
+++ src/offer/content1.java
@@ -0,0 +1,31 @@
+package offer;
+
+/**
+ * Created by ghb on 2016/12/22.
+ */
+public class content1 {
+    public static boolean find(int[][] array, int num) {
+        boolean result = false;
+        int rows = array.length;//行数
+        int columns = array[0].length;//列数
+        int row = 0;
+        int column = columns - 1;//按数组显示
+        while (row < rows && column >= 0) {
+            if (array[row][column] == num) {
+                result = true;
+                break;
+            } else if (array[row][column] < num){
+                row++;
+            }else {
+                column--;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int a[][] = {{ 1, 2, 8, 9 }, { 2, 4, 9, 12 }, { 4, 7, 10, 13 }, { 6, 8, 11, 15 }};
+        System.out.println(find(a,7));
+    }
+}
---
+++ src/offer/content2.java
@@ -0,0 +1,19 @@
+package offer;
+
+/**
+ * 替换空格
+ * Created by ghb on 2016/12/22.
+ */
+public class content2 {
+    public static void main(String[] args) {
+
+        System.out.printf("nihao");
+        for (int i = 1; i < 100; i++){
+            System.out.printf("ceshi");
+            System.out.printf("ceshi");
+            System.out.printf("ceshi");
+            System.out.printf("ceshi");
+
+        }
+    }
+}
